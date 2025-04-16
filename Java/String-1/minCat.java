cd5d8c57286b21f6d14bb945daec74c5a9edb97d
Sunny <addrosity.yt@gmail.com>
2025-04-16 12:48:51 -0400

    Create minCat.java

---
+++ Java/String-1/minCat.java
@@ -0,0 +1,8 @@
+public String minCat(String a, String b) {
+  if(a.length() > b.length()) {
+    a = a.substring(a.length() - b.length());
+  } else if(a.length() < b.length()) {
+    b = b.substring(b.length() - a.length());
+  }
+  return a + b;
+}
