ffde5f95f1ba41f8b63351bbaef48f0dc08d03fb
Sunny <addrosity.yt@gmail.com>
2025-05-03 22:41:53 -0400

    Create front11.java

---
+++ Java/Array-1/front11.java
@@ -0,0 +1,6 @@
+public int[] front11(int[] a, int[] b) {
+  if(a.length > 0 && b.length > 0) return new int[] {a[0], b[0]};
+  if(a.length > 1 && !(b.length > 1)) return new int[] {a[0]};
+  if(!(a.length > 1) && b.length > 1) return new int[] {b[0]};
+  return new int[] {};
+}
