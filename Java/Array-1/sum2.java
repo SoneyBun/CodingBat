fb81aaff3cc8ade9225003fdbce7a3ceb6860ad1
Sunny <addrosity.yt@gmail.com>
2025-05-03 22:39:22 -0400

    Update sum2.java

--- Java/Array-1/sum2.java
+++ Java/Array-1/sum2.java
@@ -1,5 +1,3 @@
 public int sum2(int[] nums) {
-  if(nums.length == 0) return 0;
-  if(nums.length == 1) return nums[0];
-  return nums[0] + nums[1];
+  return (nums.length == 0) ? 0 : (nums.length == 1) ? nums[0] : nums[0] + nums[1];
 }
