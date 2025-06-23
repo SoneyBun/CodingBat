34886bdc2c525727c1ef00b30bd9a39929e615fa
Sunny <addrosity.yt@gmail.com>
2025-06-22 23:09:59 -0400

    Create starOut.java

---
+++ Java/String-2/starOut.java
@@ -0,0 +1,10 @@
+public String starOut(String str) {
+  String s = "";
+  for(int i = 0; i < str.length(); i++) {
+    if (str.charAt(i) == '*') continue;
+    if (i > 0 && str.charAt(i - 1) == '*') continue;
+    if(i < str.length() - 1 && str.charAt(i + 1) == '*') continue;
+    s += str.charAt(i);
+  }
+  return s;
+}
